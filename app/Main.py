a9bdfa05f7e16739ba8371273a2558468816e4d0
Samoth69 <thomasviolent@gmail.com>
2023-03-18 17:34:01 +0100

    add mqtt client logging info

--- app/Main.py
+++ app/Main.py
@@ -6,6 +6,11 @@ import paho.mqtt.client as mqtt
 
 from Parser import Parser
 
+
+def on_log(client, userdata, level, buff):
+    logging.info(level, buff)
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logging.info("starting")
@@ -13,10 +18,12 @@ if __name__ == "__main__":
     ti = Parser()
     client = mqtt.Client("teleinfo")
     client.enable_logger()
+    client.on_log = on_log
     client.connect("192.168.0.7")
     client.loop_start()
     # print(json.dumps(ti.get_frame(), indent=2, separators=(',', ':')))
     while True:
+        logging.info("ping")
         dic = ti.get_frame()
         dic["_time"] = int(time.time())  # epoch
         client.publish("homeassistant/sensor/teleinfo/linky", payload=json.dumps(dic), retain=True)

d5e40b4a8935dedde3c7eae914b61bb7e726f74c
Samoth69 <thomasviolent@gmail.com>
2023-03-18 15:28:24 +0100

    le main.py

--- app/Main.py
+++ app/Main.py
@@ -1,12 +1,11 @@
 import json
 import logging
 
-import HwVendors
 from Parser import Parser
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.info("test")
 
-    ti = Parser(HwVendors.Teleinfo3())
+    ti = Parser()
     print(json.dumps(ti.get_frame(), indent=2, separators=(',', ':')))
